6b82fcf96ca5440e6641040b57752e4273958e99
Viktorya <79201287827@yandex.ru>
2023-03-23 23:52:30 +0300

    Задача 2

--- Program.cs
+++ Program.cs
@@ -2,18 +2,19 @@
 int n1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число: ");
 int n2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число: ");
-int n3 = Convert.ToInt32(Console.ReadLine());
 int max = n1;
+int min = n1;
 
 if (n2 > max)
 {
     max = n2;
 }
-if (n3 > max)
+if (n2 < min)
 {
-    max = n3;
+    min = n2;
 }
 
 Console.Write ("max=");
 Console.WriteLine (max);
+Console.Write ("min=");
+Console.WriteLine (min);
