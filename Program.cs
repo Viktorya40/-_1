ec3b026b4e31c55d5f8f47bb82dcd4fccd5f0bb3
Viktorya <79201287827@yandex.ru>
2023-03-23 23:50:19 +0300

    Задача 4

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int n3 = Convert.ToInt32(Console.ReadLine());
+int max = n1;
+
+if (n2 > max)
+{
+    max = n2;
+}
+if (n3 > max)
+{
+    max = n3;
+}
+
+Console.Write ("max=");
+Console.WriteLine (max);
