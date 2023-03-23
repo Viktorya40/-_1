184cfedae0a397a1994d9846938eb0e8b540b7a8
Viktorya <79201287827@yandex.ru>
2023-03-23 23:54:25 +0300

    Задача 6

--- Program.cs
+++ Program.cs
@@ -1,20 +1,7 @@
 ﻿Console.Write("Введите число: ");
 int n1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число: ");
-int n2 = Convert.ToInt32(Console.ReadLine());
-int max = n1;
-int min = n1;
 
-if (n2 > max)
-{
-    max = n2;
-}
-if (n2 < min)
-{
-    min = n2;
-}
-
-Console.Write ("max=");
-Console.WriteLine (max);
-Console.Write ("min=");
-Console.WriteLine (min);
+if (n1 % 2 == 0)
+    Console.Write("Четное");
+else
+    Console.Write("Не четное");
